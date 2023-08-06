cf9b37890da4e2613175a37af504cbaedc6187b2
Rediet Kifle <redietkfle@gmail.com>
2023-08-06 10:56:06 +0300

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,1 +1,25 @@
+$(document).ready(function() {
+  const amenities = {};
 
+  function updateAmenities() {
+    const amenitiesList = object.values(amenities).map(amenity => amenity.name);
+    const amenitiesText = amenitiesList.join(', ');
+
+    $('#api_status').removeClass('available');
+    $('div.amenities h4').html(amenitiesText);
+  }
+
+  $('li input[type="checkbox"]').change(function() {
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = { id: amenityId, name: amenityName };
+    } else {
+      delete amenities[amenityId];
+    }
+
+    updateAmenities();
+
+  });
+});
